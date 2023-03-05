170273d75e02f263e79b57022652d8b97e6a595b
diana nica <themindhelp@gmail.com>
2023-03-05 21:03:45 +0000

    updated components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
+import getFormattedWeatherData from "./services/weatherService";
 
 export default function App() {
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "london" });
+    console.log(data);
+  };
+
+  fetchWeather();
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
@@ -17,6 +25,7 @@ export default function App() {
       <TemperatureAndDetails />
 
       <Forecast title="hourly forecast" />
+      <Forecast title="daily forecast" />
     </div>
   );
 }
