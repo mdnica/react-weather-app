beaa681185dc9ac84bf90fc720062e7bf3d8d928
diana nica <themindhelp@gmail.com>
2023-03-08 17:00:55 +0000

    cleaned final code

--- src/App.js
+++ src/App.js
@@ -7,25 +7,47 @@ import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from "./components/TemperatureAndDetails";
 import Forecast from "./components/Forecast";
 import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "london" });
-    console.log(data);
+  const [query, setQuery] = useState({ q: "london" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      await getFormattedWeatherData({ ...query, units }).then((data) => {
+        setWeather(data);
+      });
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
   };
 
-  fetchWeather();
-
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
-      <TopButtons />
-      <Inputs />
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
+      <TopButtons setQuery={setQuery} />
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
 
-      <TimeAndLocation />
-      <TemperatureAndDetails />
+      {weather && (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
 
-      <Forecast title="hourly forecast" />
-      <Forecast title="daily forecast" />
+          <Forecast title="hourly forecast" items={weather.hourly} />
+          <Forecast title="daily forecast" items={weather.daily} />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { formatToLocalTime } from "../services/weatherService";
 
-function TimeAndLocation() {
+function TimeAndLocation({ weather: { dt, timezone, name, country } }) {
   return (
     <div>
       <div className="flex items-center justify-center my-6">
         <p className="text-white text-xl font-extralight">
-          Saturday, 04 March 2023 | Local time: 5:52 PM{" "}
+          {formatToLocalTime(dt, timezone)}
         </p>
       </div>
+
       <div className="flex items-center justify-center my-3">
-        <p className="text-white text-3xl font-medium">London, UK</p>
+        <p className="text-white text-3xl font-medium">{`${name}, ${country}`}</p>
       </div>
     </div>
   );
